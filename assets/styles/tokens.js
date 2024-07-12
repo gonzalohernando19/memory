38d1663bccb1b24b14ff044402deee8b854f57ff
gonzalo.hernando <gonzalo.hernando@sngular.com>
2024-07-12 12:31:12 +0200

    feat: add style tokens

---
+++ assets/styles/tokens.js
@@ -0,0 +1,26 @@
+import { css } from 'lit';
+
+export default css`
+  :root {
+    --memory-primary-color: #061b2b;
+    --memory-secondary-color: #ffffff;
+    --memory-tertiary-color: #3498db;
+    --memory-error-color: #cb0000;
+    --memory-success-color: #28a745;
+    --memory-font-display-default-family: sans-serif;
+    --memory-font-display-sm-size: 1rem;
+    --memory-font-display-md-size: 1.25rem;
+    --memory-font-display-lg-size: 1.75rem;
+    --memory-font-display-xl-size: 2.5rem;
+    --memory-font-display-sm-line-height: 1.5rem;
+    --memory-font-display-md-line-height: 1.75rem;
+    --memory-font-display-lg-line-height: 2rem;
+    --memory-font-display-xl-line-height: 2.5rem;
+    --memory-size-spacing-sm: 0.75rem;
+    --memory-size-spacing-md: 1.25rem;
+    --memory-size-spacing-lg: 1.5rem;
+    --memory-size-border-radius-sm: 0.5rem;
+    --memory-size-border-radius-xl: 100%;
+    --memory-size-border-width: 0.063rem;
+  }
+`;
--- src/memory-app.js
+++ src/memory-app.js
@@ -1,10 +1,14 @@
 import { LitElement, html } from 'lit';
+
 import { Router } from '@vaadin/router';
+import tokens from '../assets/styles/tokens.js';
 
 import './views/home/home.js';
 import './views/game/game.js';
 
 class Memory extends LitElement {
+  static styles = [tokens];
+
   firstUpdated() {
     super.firstUpdated();
     // const router = new Router(this.shadowRoot.querySelector('#outlet'), { baseDir: '/rock-paper-scissors' });
